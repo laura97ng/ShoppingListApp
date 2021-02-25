b8f95085f9714403e51f16f224cf603c2a9352ef
laura97ng <laura.nguyen1236@yahoo.de>
2021-02-25 18:49:52 +0100

    Test

--- app/src/main/java/de/hsh/studentproject/shoppinglistapp/MainActivity.java
+++ app/src/main/java/de/hsh/studentproject/shoppinglistapp/MainActivity.java
@@ -3,13 +3,22 @@ package de.hsh.studentproject.shoppinglistapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton addItemBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//himklad
+
+        addItemBtn = findViewById(R.id.addItemBtn);
+
+        //addItemBtn.setOnClickListener();
+
     }
 }
