73ebfeb6a210af2aec354a829521c9130c6a6036
laura97ng <laura.nguyen1236@yahoo.de>
2021-02-25 17:17:10 +0100

    Initial commit

---
+++ app/src/main/java/de/hsh/studentproject/shoppinglistapp/MainActivity.java
@@ -0,0 +1,15 @@
+package de.hsh.studentproject.shoppinglistapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
